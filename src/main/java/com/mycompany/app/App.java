9f241ce7105706501c1147467c87056b6f9ba266
rtboots <ralph@synfonix.com>
2024-03-15 12:02:39 +0000

    Bla bla

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Goodbye World!";
+    private static final String MESSAGE = "Goodbye Stupid World!";
 
     public App() {}
 
